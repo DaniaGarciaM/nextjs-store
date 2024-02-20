48e8d7d98ade433c4233079c8f4c4d1e7b2765a4
Dania Garcia <daniagm75@gmail.com>
2024-02-20 09:17:33 -0600

    1.6 Cómo funciona la navegación en Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <nav>
+            <ul>
+              <Link href="/">
+              <li>Home</li>
+              </Link>
+              <Link href="/store">
+              <li>Store</li>
+              </Link>
+            </ul>
+          </nav>
+        </header>
+        {children}</body>
     </html>
   );
 }
