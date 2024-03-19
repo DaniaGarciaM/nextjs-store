4160761d37369c0b93c6cebfb3a5e669a9929136
Dania Garcia <daniagm75@gmail.com>
2024-03-19 09:36:55 -0600

    4.29 Proyecto: implementando la página de tienda

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,3 +1,6 @@
+import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
+import { getProducts } from "app/services/shopify"
+
 interface CategoriesProps {
     params: {
         categories: string[]
@@ -5,10 +8,11 @@ interface CategoriesProps {
     }
 }
 
-export default function Categories(props: CategoriesProps){
+export default async function Categories(props: CategoriesProps){
+    const products = await getProducts()
     const { categories } = props.params
     // throw new Error('Error:Boom!')
     return(
-        <h1>Categoría dinámica {categories}</h1>
+        <ProductsWrapper products={products}/>
     );
 }
