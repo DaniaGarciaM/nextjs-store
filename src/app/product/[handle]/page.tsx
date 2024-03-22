4abc97d4fabaee2e753ebbc8a88a4295996ecdbc
Dania Garcia <daniagm75@gmail.com>
2024-03-22 10:16:06 -0600

    4.36 Cómo hacer redirects en Next.js

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,14 +1,21 @@
 import { ProductView } from "app/components/product/ProductView"
 import { getProducts } from "app/services/shopify/products"
+import { redirect } from "next/navigation"
 
 interface ProductPageProps {
-    searchParams: {
-        id: string
-    }
+  searchParams: {
+    id: string
+  }
 }
-export default async function ProductPage({ searchParams }: ProductPageProps){
-    const  id = searchParams.id
-    const products = await getProducts(id)
-    const product = products[0]
-    return <ProductView product={product} />
+
+export default async function ProductPage({ searchParams }: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+
+  if(!id){
+    redirect('/')
+  }
+
+  return <ProductView product={product} />
 }
